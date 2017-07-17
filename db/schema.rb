571d2bfea67d554d8b8d877e60f281cff2f82af2
aartivermansds <vermaaarti1996@gmail.com>
2017-07-17 07:54:03 +0530

    working on article app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717021521) do
+ActiveRecord::Schema.define(version: 20170717022203) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "deescription"
+    t.integer  "category_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "articles", ["category_id"], name: "index_articles_on_category_id"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
