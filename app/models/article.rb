d394b86d2494a9b3e348b46f6a18e0f8b84e2343
aartivermansds <vermaaarti1996@gmail.com>
2017-07-21 08:25:32 +0530

    association between user and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
-	validates :title,:description,:presence => true 
+  validates :title,:description,:presence => true 
   belongs_to :category
+  belongs_to :user
 end
